dada847b2b6757fac3b61454c5ce5bbd8c35d89b
macc2020 <makose00@gmail.com>
2020-08-01 15:12:47 -0500

    merhaba

--- src/demo/Demo1.java
+++ src/demo/Demo1.java
@@ -1,10 +1,7 @@
 package demo;
 
-public class Demo {
-
-
+public class Demo1 {
     public static void main(String[] args) {
-        System.out.println(" refwergwerg");
+        System.out.println("sdfsdfsd");
     }
-
 }
